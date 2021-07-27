37464fff2656e45ed71906d2c107602d892e97e1
YAZURA <75927232+YAZURA-SHANKHDHAR@users.noreply.github.com>
2021-07-27 18:04:10 +0530

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -7,21 +7,20 @@ function reset(){
     c=0;
 }
 function box(el){
+    k++;
     if(c==0){
         document.getElementById(el.id).innerHTML="O";
         c++;
-        chk();
     }
-    else if(c==1){
+    else{
         document.getElementById(el.id).innerHTML="X";
         c--;
-        chk();
     }
     if(chk()){
         if(c==1){
             alert("Player-O WON !!");
         }
-        else if(c==0){
+        else{
             alert("Player-X WON !!");
         }
         reset();
@@ -30,56 +29,71 @@ function box(el){
         alert("DRAW !!");
         reset();
     }
-    k++;
 }
 function chk(){
-    if(document.getElementById("p1").innerHTML!="" && document.getElementById("p2").innerHTML!="" && document.getElementById("p3").innerHTML!="" && document.getElementById("p1").innerHTML==document.getElementById("p2").innerHTML && document.getElementById("p2").innerHTML==document.getElementsById("p3").innerHTML){
-        document.getElementById("p1").style.color="blue";
-        document.getElementById("p2").style.color="blue";
-        document.getElementById("p3").style.color="blue";
+    if(document.getElementById('p1').innerHTML!="" && document.getElementById('p2').innerHTML!="" && document.getElementById('p3').innerHTML!=""
+    && document.getElementById('p1').innerHTML == document.getElementById('p2').innerHTML && document.getElementById('p2').innerHTML == document.getElementById('p3').innerHTML)
+    {
+        document.getElementById('p1').style.color = "green";
+        document.getElementById('p2').style.color = "green";
+        document.getElementById('p3').style.color = "green";
+        return true;
+
+    }
+    if(document.getElementById('p3').innerHTML!="" && document.getElementById('p6').innerHTML!="" && document.getElementById('p9').innerHTML!=""
+    && document.getElementById('p3').innerHTML == document.getElementById('p6').innerHTML && document.getElementById('p6').innerHTML == document.getElementById('p9').innerHTML)
+    {
+        document.getElementById('p3').style.color = "green";
+        document.getElementById('p6').style.color = "green";
+        document.getElementById('p9').style.color = "green";
         return true;
     }
-    if(document.getElementById("p4").innerHTML!="" && document.getElementById("p5").innerHTML!="" && document.getElementById("p6").innerHTML!="" && document.getElementById("p4").innerHTML==document.getElementById("p5").innerHTML && document.getElementById("p6").innerHTML==document.getElementsById("p3").innerHTML){
-        document.getElementById("p4").style.color="blue";
-        document.getElementById("p5").style.color="blue";
-        document.getElementById("p6").style.color="blue";
+    if(document.getElementById('p7').innerHTML!="" && document.getElementById('p8').innerHTML!="" && document.getElementById('p9').innerHTML!=""
+    && document.getElementById('p7').innerHTML == document.getElementById('p8').innerHTML && document.getElementById('p8').innerHTML == document.getElementById('p9').innerHTML)
+    {
+        document.getElementById('p7').style.color = "green";
+        document.getElementById('p8').style.color = "green";
+        document.getElementById('p9').style.color = "green";
         return true;
     }
-    if(document.getElementById("p7").innerHTML!="" && document.getElementById("p8").innerHTML!="" && document.getElementById("p9").innerHTML!="" && document.getElementById("p7").innerHTML==document.getElementById("p8").innerHTML && document.getElementById("p9").innerHTML==document.getElementsById("p3").innerHTML){
-        document.getElementById("p7").style.color="blue";
-        document.getElementById("p8").style.color="blue";
-        document.getElementById("p9").style.color="blue";
+    if(document.getElementById('p1').innerHTML!="" && document.getElementById('p4').innerHTML!="" && document.getElementById('p7').innerHTML!=""
+    && document.getElementById('p1').innerHTML == document.getElementById('p4').innerHTML && document.getElementById('p4').innerHTML == document.getElementById('p7').innerHTML)
+    {
+        document.getElementById('p1').style.color = "green";
+        document.getElementById('p4').style.color = "green";
+        document.getElementById('p7').style.color = "green";
         return true;
     }
-    if(document.getElementById("p1").innerHTML!="" && document.getElementById("p4").innerHTML!="" && document.getElementById("p7").innerHTML!="" && document.getElementById("p1").innerHTML==document.getElementById("p4").innerHTML && document.getElementById("p7").innerHTML==document.getElementsById("p3").innerHTML){
-        document.getElementById("p1").style.color="blue";
-        document.getElementById("p4").style.color="blue";
-        document.getElementById("p7").style.color="blue";
+    if(document.getElementById('p1').innerHTML!="" && document.getElementById('p5').innerHTML!="" && document.getElementById('p9').innerHTML!=""
+    && document.getElementById('p1').innerHTML == document.getElementById('p5').innerHTML && document.getElementById('p5').innerHTML == document.getElementById('p9').innerHTML)
+    {
+        document.getElementById('p1').style.color = "green";
+        document.getElementById('p5').style.color = "green";
+        document.getElementById('p9').style.color = "green";
         return true;
     }
-    if(document.getElementById("p2").innerHTML!="" && document.getElementById("p5").innerHTML!="" && document.getElementById("p8").innerHTML!="" && document.getElementById("p2").innerHTML==document.getElementById("p5").innerHTML && document.getElementById("p8").innerHTML==document.getElementsById("p3").innerHTML){
-        document.getElementById("p2").style.color="blue";
-        document.getElementById("p5").style.color="blue";
-        document.getElementById("p8").style.color="blue";
+    if(document.getElementById('p3').innerHTML!="" && document.getElementById('p5').innerHTML!="" && document.getElementById('p7').innerHTML!=""
+    && document.getElementById('p3').innerHTML == document.getElementById('p5').innerHTML && document.getElementById('p5').innerHTML == document.getElementById('p7').innerHTML)
+    {
+        document.getElementById('p3').style.color = "green";
+        document.getElementById('p5').style.color = "green";
+        document.getElementById('p7').style.color = "green";
         return true;
     }
-    if(document.getElementById("p3").innerHTML!="" && document.getElementById("p6").innerHTML!="" && document.getElementById("p9").innerHTML!="" && document.getElementById("p3").innerHTML==document.getElementById("p6").innerHTML && document.getElementById("p9").innerHTML==document.getElementsById("p3").innerHTML){
-        document.getElementById("p3").style.color="blue";
-        document.getElementById("p6").style.color="blue";
-        document.getElementById("p9").style.color="blue";
+    if(document.getElementById('p2').innerHTML!="" && document.getElementById('p5').innerHTML!="" && document.getElementById('p8').innerHTML!=""
+    && document.getElementById('p2').innerHTML == document.getElementById('p5').innerHTML && document.getElementById('p5').innerHTML == document.getElementById('p8').innerHTML)
+    {
+        document.getElementById('p2').style.color = "green";
+        document.getElementById('p5').style.color = "green";
+        document.getElementById('p8').style.color = "green";
         return true;
     }
-    if(document.getElementById("p1").innerHTML!="" && document.getElementById("p5").innerHTML!="" && document.getElementById("p9").innerHTML!="" && document.getElementById("p1").innerHTML==document.getElementById("p5").innerHTML && document.getElementById("p9").innerHTML==document.getElementsById("p3").innerHTML){
-        document.getElementById("p1").style.color="blue";
-        document.getElementById("p5").style.color="blue";
-        document.getElementById("p9").style.color="blue";
+    if(document.getElementById('p4').innerHTML!="" && document.getElementById('p5').innerHTML!="" && document.getElementById('p6').innerHTML!=""
+    && document.getElementById('p4').innerHTML == document.getElementById('p5').innerHTML && document.getElementById('p5').innerHTML == document.getElementById('p6').innerHTML)
+    {
+        document.getElementById('p4').style.color = "green";
+        document.getElementById('p5').style.color = "green";
+        document.getElementById('p6').style.color = "green";
         return true;
     }
-    if(document.getElementById("p3").innerHTML!="" && document.getElementById("p5").innerHTML!="" && document.getElementById("p7").innerHTML!="" && document.getElementById("p3").innerHTML==document.getElementById("p5").innerHTML && document.getElementById("p7").innerHTML==document.getElementsById("p3").innerHTML){
-        document.getElementById("p3").style.color="blue";
-        document.getElementById("p5").style.color="blue";
-        document.getElementById("p7").style.color="blue";
-        return true;
-    }
-    
 }
